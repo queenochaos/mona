220b58a8a5eb8d1ad0248ba1784782548b3308c3
Neko Of The Abyss <thenekooftheabyss@gmail.com>
2022-05-16 22:39:03 +0530

    util functions

---
+++ src/util.ts
@@ -0,0 +1,121 @@
+export const AddCommas = (x: number): string => {
+  const y = x ? x.toString().split(".") : [""];
+  y[0] = y[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  return y.join(".");
+};
+
+export const AddZero = (num: number, digits = 3): string => {
+  const arr = new Array(digits).fill(0);
+  return `${arr.join("").slice(0, 0 - num.toString().length)}${num}`;
+};
+
+
+export const ColorFromHex = (color: string | number[]): number => {
+    if (!color) return Math.floor(Math.random() * (0xffffff + 1));
+    let newColor = 0;
+    if (typeof color === "string") {
+      newColor = parseInt(color.replace("#", ""), 16);
+    } else if (Array.isArray(color)) {
+      newColor = (color[0] << 16) + (color[1] << 8) + color[2];
+    }
+  
+    if (newColor < 0 || newColor > 0xffffff) throw new RangeError("COLOR_RANGE");
+    else if (Number.isNaN(newColor)) throw new TypeError("COLOR_CONVERT");
+  
+    return newColor;
+  };
+
+export class BetterMap<K, V> extends Map<K, V> {
+    name: string;
+    constructor(name: string) {
+      super();
+      this.name = name;
+    }
+    array() {
+      return this.map((x) => x);
+    }
+    every(fn: (v: V, k: K) => boolean): boolean {
+      for (const [k, v] of this.entries()) {
+        if (!fn(v, k)) {
+          return false;
+        }
+      }
+      return true;
+    }
+    filter(fn: (v: V, k: K) => boolean): BetterMap<K, V> {
+      const newColl = new BetterMap<K, V>(this.name);
+      for (const [key, val] of this.entries()) {
+        if (fn(val, key)) {
+          newColl.set(key, val);
+        }
+      }
+      return newColl;
+    }
+    find(fn: (v: V, k: K) => boolean): V | undefined {
+      for (const [k, v] of this.entries()) {
+        if (fn(v, k)) {
+          return v;
+        }
+      }
+      return undefined;
+    }
+    first(): V | undefined {
+      return this.values().next().value;
+    }
+    map<T>(fn: (v: V, k: K) => T): T[] | [] {
+      const arr = [];
+      for (const [k, v] of this.entries()) {
+        arr.push(fn(v, k));
+      }
+      return arr;
+    }
+    random(): V | undefined {
+      const max = Math.floor(Math.random() * this.size);
+      const iter = this.values();
+      for (let i = 0; i < max; ++i) {
+        iter.next();
+      }
+      return iter.next().value;
+    }
+    reduce<T>(fn: (acc: T, val: V) => T, first: T): T {
+      const iter = this.values();
+      let val;
+      let result = first === undefined ? iter.next().value : first;
+      while ((val = iter.next().value) !== undefined) {
+        result = fn(result, val);
+      }
+      return result;
+    }
+    some(fn: (val: V, key: K) => boolean): boolean {
+      for (const [k, v] of this.entries()) {
+        if (fn(v, k)) {
+          return true;
+        }
+      }
+      return false;
+    }
+    sort(fn: (v1: V, v2: V, k1: K, k2: K) => number = () => 0): BetterMap<K, V> {
+      const items = [...this.entries()];
+      items.sort((a, b) => fn(a[1], b[1], a[0], b[0]));
+      const newColl = new BetterMap<K, V>(this.name);
+      for (const [k, v] of items) {
+        newColl.set(k, v);
+      }
+      return newColl;
+    }
+    toString(): string {
+      return `[BetterMap of <${this.name}>]`;
+    }
+    json(): Record<string, unknown> {
+      const json = {};
+      for (const item of this.entries()) {
+        Object.defineProperty(json, `${item[0]}`, { value: item[1], writable: true });
+      }
+      return json;
+    }
+    toJSON(): Record<string, unknown> {
+      return this.json()
+    }
+  }
+  
+  
